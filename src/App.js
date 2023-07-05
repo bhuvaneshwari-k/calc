95e4b2062fb69f8b93f3fd078ed913238846e9d4
bhuvaneshwari.k@tringapps.com <bhuvaneshwari.k@tringapps.com>
2023-07-05 19:00:33 +0530

    Commit message describing the changes

--- src/App.js
+++ src/App.js
@@ -25,24 +25,24 @@ function App() {
         <input type="text" value={result}></input>
       </div>
       <div className="keyboard">
-          <button onClick={clear} >Clear</button>
-          <button onClick={backSpace}>C</button>
-          <button name="+" onClick={handleClick}>+</button>
+          <button id='spcl'className="Clear" onClick={clear} >Clear</button>
+          <button id='spcl'className="bs" onClick={backSpace}>C</button>
+          <button id='spcl'name="+" onClick={handleClick}>+</button>
           <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
-          <button name="-" onClick={handleClick}>-</button>
+          <button id='spcl'name="-" onClick={handleClick}>-</button>
           <button name="4" onClick={handleClick}>4</button>
           <button name="5" onClick={handleClick}>5</button>
           <button name="6" onClick={handleClick}>6</button>
-          <button name="*" onClick={handleClick}>*</button>
+          <button id='spcl'name="*" onClick={handleClick}>*</button>
           <button name="7" onClick={handleClick}>7</button>
           <button name="8" onClick={handleClick}>8</button>
           <button name="9" onClick={handleClick}>9</button>
-          <button name="/" onClick={handleClick}>/</button>
+          <button id='spcl'name="/" onClick={handleClick}>/</button>
           <button name="0" onClick={handleClick}>0</button>
           <button name="." onClick={handleClick}>.</button>
-          <button name="=" onClick={calculate}>=</button>
+          <button id='spcl'className="result" name="=" onClick={calculate}>=</button>
       </div>
     </div>
   );
